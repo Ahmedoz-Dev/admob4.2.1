c282c3d029ebd7f56e6a9d79ccdbf0981c588587
Ahmedoz-Dev <134301105+Ahmedoz-Dev@users.noreply.github.com>
2024-04-19 14:50:36 +0300

    Add files via upload

---
+++ Main.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _on_banner_pressed():
+	pass # Replace with function body.
+
+
+func _on_reward_pressed():
+	pass # Replace with function body.
+
+
+func _on_interstitial_pressed():
+	pass # Replace with function body.
